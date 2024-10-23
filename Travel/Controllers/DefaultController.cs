29ab06b0b9ade27d08b4522111d1c75f1f0204ee
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-10-24 00:25:53 +0300

    Thema

---
+++ Travel/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Travel.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //Controller yapısına gelen isteklere göre işlem yapıp kullanıcıya View ile isteğe göre bilgileri geri döndüren metotlara verilen isimdir.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
