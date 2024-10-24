2374ed9321cca63ac1384a88bc450368f9992481
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-10-24 18:21:52 +0300

    About and Blog

---
+++ Travel/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Travel.Models.Sınıflar;
+
+namespace Travel.Controllers
+{
+	public class AboutController : Controller
+	{
+		private readonly Context _context;
+
+		public AboutController(Context context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+		{
+			var degerler = _context.Hakkimizdas.ToList();
+			return View(degerler);
+		}
+	}
+}
---
+++ Travel/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Travel.Models.Sınıflar;
+
+namespace Travel.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly Context _context;
+
+        public BlogController(Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var bloglar = _context.Blogs.ToList();
+            return View(bloglar);
+        }
+    }
+}
