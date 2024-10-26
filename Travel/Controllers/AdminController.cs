3d3f423911ff5003e31d27953cabbf5c83d9de89
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-10-27 00:54:20 +0300

    Admin

--- Travel/Controllers/AdminController.cs
+++ Travel/Controllers/AdminController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Travel.Models.Sınıflar;
 
 namespace Travel.Controllers
 {
     public class AdminController : Controller
     {
+        //Dependy Injection
+        private readonly Context _context;
+
+        //ctor
+        public AdminController(Context context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var degerler = _context.Blogs.ToList();
             return View();
         }
     }
